f4f87abdb4a375d26d8104f91089cc8f81b7dbd1
eeveelise <evelise.dib@loft.com.br>
2020-11-06 18:29:44 -0300

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wendy';
+  title = "wendy";
+  isDisabled = false;
+  urlAvatar = "https://avatars2.githubusercontent.com/u/10232707?s=460&u=464e1e65dfb54dcebfd508e7697e123963e776b7&v=4";
+
+  buttonClick(){
+    console.log('Clicou no botão');
+  }
 }
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wn-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
