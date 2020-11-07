4217d3b0a270d82e625906c9aa187d38c48fc9f7
eeveelise <evelise.dib@loft.com.br>
2020-11-07 10:16:49 -0300

    adicionando get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,37 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 valueInput = "";
-isHidden = false;
+private _isHidden = false;
 d20 = null;
+form = {
+  nome: '',
+  raca: '',
+  classe: '',
+  nivel: '',
+  telefone: ''
+}
+
+  get isHidden() {
+    return this._isHidden;
+  }
 
   getValue(event: InputEvent){
+    //casting
+    // const evento = event.target as HTMLTextAreaElement;
     const evento = <HTMLTextAreaElement>event.target;
     this.valueInput = evento.value;
   }
   
   favoritar() {
-    this.isHidden = !this.isHidden;
+    this._isHidden = !this._isHidden;
   }
 
   calcD20() {
     this.d20 = Math.floor((Math.random() * 20) + 1);
+  }
+
+  getFormValue(event: InputEvent, campo: string) {
+    const evento = <HTMLTextAreaElement>event.target;
+    this.form[campo] = evento.value;
   }
 }
